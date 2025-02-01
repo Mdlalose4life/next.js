61988e84697c25f4ca94f8f7912b29689dea74b8
sbusisoShaper <sbusisomdlalose@thedigitalacademy.co.za>
2025-02-02 00:42:24 +0200

    Foodies

---
+++ next-crush-course/foodies/app/meals/[slug]/page.js
@@ -0,0 +1,8 @@
+function MealDetailsPage({params}){
+    return (
+        <h1>
+            This is the meal details Page
+        </h1>
+    )
+}
+export default MealDetailsPage
---
+++ next-crush-course/foodies/app/meals/share/page.js
@@ -0,0 +1,8 @@
+function mealsSharePage(){
+    return(
+        <h1>
+            Meals share
+        </h1>
+    )
+}
+export default mealsSharePage
