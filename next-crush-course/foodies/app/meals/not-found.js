5019e3d0c9fadd3d5e9a8ffdf05e20e4b7962012
sbusisoShaper <sbusisomdlalose@thedigitalacademy.co.za>
2025-02-20 12:45:40 +0200

    adding error handling

---
+++ next-crush-course/foodies/app/meals/not-found.js
@@ -0,0 +1,6 @@
+export default function NotFound(){
+    return <main className="not-found">
+        <h1>Meal not Found!</h1>
+        <p>Unfortunatly, we could not fin the requested page or meals.</p>
+    </main>
+}
